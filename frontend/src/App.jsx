55e3ab2fcd18a6d327d2158ded08493d7d70a38c
Yash Raghubanshi <Yash990-bit@users.noreply.github.com>
2026-04-27 11:51:52 +0530

    fix: removed PredictSymptoms reference to fix build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Home from './pages/Home'
 import PredictDiabetes from './pages/PredictDiabetes'
 import PredictHeart from './pages/PredictHeart'
 import PredictLiver from './pages/PredictLiver'
-import PredictSymptoms from './pages/PredictSymptoms'
 import History from './pages/History'
 import About from './pages/About'
 
@@ -21,7 +20,6 @@ function App() {
                         <Route path="/predict/diabetes" element={<PredictDiabetes />} />
                         <Route path="/predict/heart" element={<PredictHeart />} />
                         <Route path="/predict/liver" element={<PredictLiver />} />
-                        <Route path="/predict/symptoms" element={<PredictSymptoms />} />
                         <Route path="/history" element={<History />} />
                         <Route path="/about" element={<About />} />
                     </Routes>
